89dce170b0d74ed5b730cb9dbda3f44f1cad0030
pianory <kev220@naver.com>
2023-06-13 22:47:24 +0900

    Physics

--- main.py
+++ main.py
@@ -1,51 +1,132 @@
 import pygame
+import objects
+import pymunk
+import pymunk.pygame_util
 import math
 
+# 스크린 크기 지정
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
 
-class Ball():
-    def __init__(self, name:int, x:float, y:float, speed:float=0.0, angle:float=0.0):
-        """
-        당구공을 형성하는 class입니다.
-        name : 당구공의 이름을 말합니다. (int : 0~15, 0은 플레이어의 공)
-        x : 현재 당구공의 x 위치를 입력합니다. (float)
-        y : 현재 당구공의 y 위치를 입력합니다. (float)
-        speed : 현재 당구공의 속도를 입력합니다. (float, 기본 0.0)
-        angle : 현재 당구공의 각도를 입력합니다. (float, 기본 0.0)
-        """
-        self.name, self.x, self.y = name, x, y
-        self.speed, self.angle = speed, angle
-        
+# pygame 초기화
+pygame.init()
 
-    def move(self):
-        """
-        당구공을 현재 방향과 속도대로 이동합니다.
-        """
-        self.x += math.cos(self.angle) * self.speed
-        self.y += math.sin(self.angle) * self.speed * (-1) # y좌표의 경우 화면 상에서 일반적인 방향의 반대 방향이므로 반대로 적어줍니다.
+# 스크린 객체 저장
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Pool Game")
 
-    def collisionWithBall(self, other):
-        """
-        당구공과 당구공간의 충돌을 계산합니다.
-        other : 다른 공을 입력합니다. (Ball)
-        return : self의 공이 충돌한 후 어떻게 될지 반환합니다. (Ball)
-        """
-        return Ball(self.name, self.x, self.y)
+# pymunk 공간
+space = pymunk.Space()
+staticBody = space.static_body
+drawOptions = pymunk.pygame_util.DrawOptions(SCREEN)
+
+# 일정한 프레임 보장
+clock = pygame.time.Clock()
+FPS = 60
+
+# 이미지 로딩
+tableImage = pygame.image.load("./data/img/board.png").convert_alpha()
+cueImage = pygame.image.load("./data/img/cue.png").convert_alpha()
+
+# 공 생성 함수
+ballRadius = 12
+def createBall(position:tuple, name:int):
+    body = pymunk.Body()
+    body.position = position
+    shape = pymunk.Circle(body, ballRadius)
+    shape.mass = 10
+    shape.elasticity = 0.95
+
+    pivot = pymunk.PivotJoint(staticBody, body, (0, 0), (0, 0))
+    pivot.max_bias = 0
+    pivot.max_force = 200 # 마찰
+
+    space.add(body, shape, pivot)
+    return shape
+
+# 쿠션 생성 함수
+def createCushion(poly_dims):
+    body = pymunk.Body(body_type=pymunk.Body.STATIC)
+    body.position = ((215, 200))
+    shape = pymunk.Poly(body, poly_dims)
+    shape.elasticity = 0.75
     
-    def collisionWithWall(self, wall:str):
-        """
-        당구공과 벽의 충돌을 계산합니다. 이 연산은 반환값이 없습니다.
-        wall : 벽의 방향을 입력합니다. (str : 'UP', 'RIGHT', 'DOWN', 'LEFT')
-        """
-        if wall == 'UP' or wall == 'DOWN':
-            self.angle = -self.angle
+    space.add(body, shape)
 
-        elif wall == 'RIGHT' or wall == 'LEFT':
-            self.angle = 2*math.pi - self.angle
+# 쿠션의 위치
+cushions = [[(51, 72), (64, 89), (64, 382), (51, 404)], 
+            [(73, 47), (92, 59), (382, 59), (403, 47)], 
+            [(777, 47), (758, 59), (468, 59), (447, 47)], 
+            [(799, 72), (786, 89), (786, 382), (799, 404)], 
+            [(73, 424), (92, 410), (382, 410), (403, 424)], 
+            [(777, 424), (758, 410), (468, 410), (447, 424)]]
 
-        else:
-            raise Exception('잘못된 벽의 이름입니다.')
-        
-    def show(self):
-        """
-        화면에 x좌표, y좌표
-        """
+
+for cushion in cushions:
+    createCushion(cushion)
+
+# 큐 생성
+class Cue():
+    def __init__(self, position):
+        self.originImage = cueImage
+        self.angle = 0
+        self.image = pygame.transform.rotate(self.originImage, self.angle)
+        self.rect = self.image.get_rect()
+        self.rect.center = position
+
+    def update(self, angle):
+        self.angle = angle
+
+    def draw(self, surface):
+        self.image = pygame.transform.rotate(self.originImage, self.angle)
+        surface.blit(self.image, self.rect)
+
+
+
+# 게임 준비
+balls = []
+rows = 5
+ballImg = [pygame.transform.scale(pygame.image.load("./data/img/Ball"+str(i)+".png"), (ballRadius*2, ballRadius*2)) for i in range(16)]
+# 공 세팅
+for col in range(5):
+    for row in range(rows):
+        pos = (420 + (col*ballRadius-2)*2, 388 + (row*(ballRadius+1))*2 + (col * ballRadius))
+        newBall = createBall(pos, 1)
+        balls.append(newBall)
+    rows -= 1
+
+playerBall = createBall((840, 438), 0)
+backgroundColor = pygame.Color('white')
+cue = Cue(playerBall.body.position)
+# Main
+
+isRunning = True
+
+while isRunning:
+    SCREEN.fill(backgroundColor)
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            playerBall.body.apply_impulse_at_local_point((-500, 0), (0, 0))
+
+        # 게임 종료    
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+    # 테이블 그리기
+    SCREEN.blit(tableImage, (215, 200))
+
+    # 공 그리기
+    for i, ball in enumerate(balls):
+        SCREEN.blit(ballImg[i+1], (ball.body.position[0]-ballRadius, ball.body.position[1]-ballRadius))
+    SCREEN.blit(ballImg[0], (playerBall.body.position[0]-ballRadius, playerBall.body.position[1]-ballRadius))
+
+    # 큐 그리기
+    # 큐 각도 계산하기
+
+    # cue.draw(SCREEN)
+
+    # space.debug_draw(drawOptions)
+
+    pygame.display.update()
+    clock.tick(FPS)
+    space.step(1/FPS)
